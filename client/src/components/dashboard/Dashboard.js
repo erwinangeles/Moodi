3fc71802cbad04d2f2bada18fd75137f81bf1155
Erwin Angeles <erwin@eang.media>
2020-01-18 09:44:36 -0600

    minimal dashboard changes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -25,7 +25,19 @@ class Dashboard extends Component {
   }
 
   summarizeMorning(){
-  
+    var store = this.state.morningMoods;
+    var frequency = {};  // array of frequency.
+    var max = 0;  // holds the max frequency.
+    var result;   // holds the max frequency element.
+    for(var v in store) {
+            frequency[store[v]]=(frequency[store[v]] || 0)+1; // increment frequency.
+            if(frequency[store[v]] > max) { // is this frequency > max so far ?
+                    max = frequency[store[v]];  // update max.
+                    result = store[v];          // update result.
+            }
+    }
+    console.log("result")
+    console.log(result);
   }
 
   summarizeEvening() {
