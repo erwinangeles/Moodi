b4463917b3b486592ab0b30b4d7a366a7212cceb
Erwin Angeles <erwin@eang.media>
2020-01-19 00:02:03 -0600

    fixed

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use(
 );
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
 }
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // DB Config
